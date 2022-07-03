df4c66fce46b313173976a7437f6645f10bc947a
Huseyin <akcali-1997@hotmail.com>
2022-07-03 10:50:57 +0300

    commit.
    Turning function okey.
    Just added Space.World in Rotate method.

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -30,18 +30,18 @@ public class CubeController : MonoBehaviour
         }
         else if (Input.GetMouseButton(0))
         {
-            // Here we are turning the cube but it turns wrong :(
+            // Everything is okey now turning function here but turning is happening only in one axis.
             activeMousePos = new Vector2(Input.mousePosition.x / Screen.width, Input.mousePosition.y / Screen.height);
             if (Mathf.Abs(activeMousePos.x - firstMousePos.x) >Mathf.Abs(activeMousePos.y - firstMousePos.y))
             {
                  rotateWay = new Vector3(0, activeMousePos.x - firstMousePos.x, 0);
-                transform.Rotate(-rotateWay * 270f * Time.deltaTime);
+                transform.Rotate(-rotateWay * 270f * Time.deltaTime,Space.World);
 
             }
             else if (Mathf.Abs(activeMousePos.y - firstMousePos.y) > Mathf.Abs(activeMousePos.x - firstMousePos.x))
             {
                  rotateWay = new Vector3(firstMousePos.y - activeMousePos.y,0 , 0);
-                transform.Rotate(-rotateWay * 270f * Time.deltaTime);
+                transform.Rotate(-rotateWay * 270f * Time.deltaTime,Space.World);
             }
             
         }
